f8d56083f8129d2f0c7210f37e887f17a5a788f3
Joris den Uijl <jorisdenuijl@gmail.com>
2019-03-03 13:45:21 +0100

    Added tests

---
+++ tests/test_MM3_queue.py
@@ -0,0 +1,19 @@
+"""Tests for queueing package."""
+
+import numpy as np
+import queueing
+
+from scipy import stats
+
+def test_MM3_queue():
+    A = queueing.arrival_process("M", stats.poisson(1))
+    S = queueing.service_process("M", stats.expon(1))
+    c = 3
+
+    q = queueing.queue(A, S, c)
+    sim = queueing.simulation(q)
+
+    sim.simulate(100000)
+
+    np.testing.assert_almost_equal(sim.environment.queue.A.arrival_rate, np.mean(sim.environment.arrivals), decimal = 3)
+    np.testing.assert_almost_equal(sim.environment.queue.S.mean_service_time, np.mean(sim.environment.service_times), decimal = 3)
