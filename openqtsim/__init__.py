81bd32dbba9d0b519761a0fb266e91a424bf6da6
Mark van Koningsveld <m.vankoningsveld@tudelft.nl>
2020-03-11 21:12:32 +0100

    Minor cleanup

--- openqtsim/__init__.py
+++ openqtsim/__init__.py
@@ -5,11 +5,9 @@ __email__ = "m.vankoningsveld@tudelft.nl"
 __version__ = "0.3.0"
 
 from .arrival_process import ArrivalProcess
-from .service_process import ServiceProcess
-
-from .queue import Queue
 from .customer import Customer
-from .simulation import Simulation
 from .mm1 import MM1
-from .tables import get_waiting_time_service_time_table
 from .mt_engine import worker, Task
+from .queue import Queue
+from .service_process import ServiceProcess
+from .simulation import Simulation
